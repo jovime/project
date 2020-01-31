a099dcfc0a3eaf82e78b4ca84082e8ed97f60cf5
jovime <48872796+jovime@users.noreply.github.com>
2020-01-31 20:09:48 +0700

    Add files via upload

---
+++ intro.php
@@ -0,0 +1,22 @@
+<?php
+
+$banner = "\e[36;1m                                                                                 
+                                                                                 
+           #         ######   
+           #    #             
+  ######   #    #  ########## 
+           #    #  # J  H  O      
+           #######        ##  
+##########      #       ##    
+                #     ##      
+                              
+                                                                                 
+[#]  Spam SMS [#]    
+                                   
+Author : JHO                  
+Team   : IndoSec                   
+Github : https//github.com/jovime/\n\e[0;1m";
+                                                                                 
+                                                                                                                                                                 
+sleep(3);
+echo $banner;
